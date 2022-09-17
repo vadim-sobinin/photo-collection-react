e46658593b34499c8f91e76a18e21c438ec60889
vadim-sobinin <vadimsobinin@yandex.ru>
2022-09-17 23:05:07 +0300

    Fix api link and render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss';
-import React, { useState, useEffect } from "react";
+import {Collection} from './Collection'
 
 let cats = [
   { "name": "All" },
@@ -10,36 +10,23 @@ let cats = [
   { "name": "Cities" }
 ];
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
-
 
 
 function App() {
 
-  const [collection, setCollection] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [categoryId, setCategoryId] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  const [collections, setCollection] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
 
   const category = categoryId ? `category=${categoryId}` : "";
 
-   useEffect(() => {
-     setIsLoading(true);
+  React.useEffect(() => {
+    //React.setIsLoading(true);
      fetch(
-       `https://6306f7cac0d0f2b801242e89.mockapi.io/photo_collections?page=${page}&limit=3&${category}`
+      // 'https://632620f270c3fa390f94c420.mockapi.io/collections'
+      `https://632620f270c3fa390f94c420.mockapi.io/collections?page=${page}&limit=3&${category}`
      )
        .then((res) => res.json())
        .then((json) => {
@@ -49,11 +36,11 @@ function App() {
          console.warn(err);
          alert("Error in receiving data");
        })
-       .finally(() => setIsLoading(false));
+       //.finally(() => React.setIsLoading(false));
    }, [categoryId, page]);
 
 
-   
+
 
 
   return (
@@ -70,15 +57,14 @@ function App() {
         <input className="search-input" placeholder="Search by title" />
       </div>
       <div className="content">
-        <Collection
-          name="Travelling the world"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {
+          collections.map((obj) => (
+            <Collection
+              name={obj.name}
+              images={obj.photos}
+            />
+          ))
+        }
       </div>
       <ul className="pagination">
         <li>1</li>
